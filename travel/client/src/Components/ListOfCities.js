81857e4233b476853e86ea2d4378ffa404afface
blockerpatrice <patricemblocker@yahoo.com>
2020-03-04 13:32:35 -0700

    Updated some CSS

--- travel/client/src/Components/ListOfCities.js
+++ travel/client/src/Components/ListOfCities.js
@@ -7,16 +7,17 @@ const ListOfCities = (props) => {
   const {name, rating, comments, _id, image} = props
     return(
         <div className="city-styles">
-          <div className="grid-item">
-          <p>{props.name}, Rating: {props.rating}</p>
-          
-          <p>Comments: {props.comments}</p>
           
           <img src={props.image} className="images"/>
+          <p className="city">{props.name}, Rating: {props.rating}</p>
+          
+          <p className="comments">Comments: {props.comments}</p>
+          
+          
           <button 
             className="button-style" 
             onClick={() => props.deleteCity(_id)}>Delete</button>
-          </div>
+          
         </div>  
 
     )
